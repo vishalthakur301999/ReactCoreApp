df712e33b8bf13d6261275abd68247850da02b2b
Vishal Thakur <vishalthakur@Vishals-MacBook-Pro.local>
2021-04-04 17:24:17 +0530

    initial

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+//using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
